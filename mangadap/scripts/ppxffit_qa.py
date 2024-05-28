210264b65294c360cf6f21c20b94a966ab8a122c
Erick Aguirre <ea49610@gmail.com>
2024-05-27 22:59:18 -0600

    changes to text font in each plot

--- mangadap/scripts/ppxffit_qa.py
+++ mangadap/scripts/ppxffit_qa.py
@@ -396,7 +396,7 @@ def stellar_continuum_maps(plt, ifu, daptype, snr, r68, r99, rchi2, signal, a, d
     ax.set_ylim(extent[2:])
     # ax.add_patch(map_beam_patch(extent, ax, facecolor='0.7', edgecolor='k', zorder=4))
     ax.text(0.1, 0.95, r'$V_\ast$ (km/s)', horizontalalignment='left',
-            verticalalignment='center', transform=ax.transAxes,zorder=4, c='r')
+            verticalalignment='center', transform=ax.transAxes,zorder=4, c='k')
 
     # ------------------------------------------------------------------
     # Stellar sigma

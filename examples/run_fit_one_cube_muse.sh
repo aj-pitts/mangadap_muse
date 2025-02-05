77ba1a50ecb37bcbd9512d89669b399d2bc26c8c
AndrewPitts1996 <apitts4030@sdsu.edu>
2025-02-05 15:31:50 -0800

    fixed issue of argument separation preservation

--- examples/run_fit_one_cube_muse.sh
+++ examples/run_fit_one_cube_muse.sh
@@ -9,6 +9,6 @@ LOGFILE="$LOGDIR/DAP_$1_$2_${TIMESTAMP}.log"
 
 mkdir -p "$LOGDIR"
 
-python fit_one_cube_muse.py "$ARGS" > "$LOGFILE" 2>&1 &
+python fit_one_cube_muse.py "$@" > "$LOGFILE" 2>&1 &
 
 echo "Logging output to $LOGFILE"

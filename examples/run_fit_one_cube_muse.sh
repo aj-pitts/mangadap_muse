02e123ebff72ec5327d938db10018b251bfc6bdb
AndrewPitts1996 <apitts4030@sdsu.edu>
2025-02-05 15:41:36 -0800

    updated log directory to contain galaxy subdirs

--- examples/run_fit_one_cube_muse.sh
+++ examples/run_fit_one_cube_muse.sh
@@ -4,10 +4,11 @@ ARGS="$@"
 
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 
-LOGDIR="DAP_Logs"
-LOGFILE="$LOGDIR/DAP_$1_$2_${TIMESTAMP}.log"
+LOGDIR="DAP_logs"
+LOGSUBDIR="$LOGDIR/$1_$2"
+LOGFILE="$LOGSUBDIR/DAP_$1_$2_${TIMESTAMP}.log"
 
-mkdir -p "$LOGDIR"
+mkdir -p "$LOGSUBDIR"
 
 python fit_one_cube_muse.py "$@" > "$LOGFILE" 2>&1 &
 

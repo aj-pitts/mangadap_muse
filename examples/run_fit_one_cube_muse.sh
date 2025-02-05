1c608e8c1042afdf12e052e77e03e0cb2a7d5726
AndrewPitts1996 <apitts4030@sdsu.edu>
2025-02-05 15:02:51 -0800

    added shell script to run the DAP

---
+++ examples/run_fit_one_cube_muse.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ARGS="$@"
+
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+
+LOGDIR="DAP_Logs"
+LOGFILE="$LOGDIR/DAP_$1_$2_${TIMESTAMP}.log"
+
+mkdir -p "$LOGDIR"
+
+python fit_one_cube_muse.py "$ARGS" > "LOGFILE" 2>&1 &
+
+echo "Logging output to $LOGFILE"
